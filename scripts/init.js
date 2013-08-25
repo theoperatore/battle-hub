01e12ba79b6eab4f6c31cd2bab9c6e8aef31488a
Alexander Petersen <theoperatore@gmail.com>
2013-08-25 10:06:17 -0500

    updated polar clock

--- scripts/init.js
+++ scripts/init.js
@@ -5,64 +5,65 @@ window.addEventListener('load', function(ev) {
 		center       = { x : (canvas.width / 2), y : (canvas.height / 2) },
 		interval     = setInterval(polar,1);
 
-    function polar() {
-    	//variables used by all arcs
-        var timeNow = new Date(),
-        	x = center.x,
-        	y = center.y,
-        	startAngle = (3*Math.PI/2),
-        	anticlockwise = false,
-        	radius,
-        	radiusMin,
-        	radiusHr,
-        	seconds,
-        	degsMin,
-        	degsHr,
-        	currRads,
-        	radsMin,
-        	radsHr,
-        	endAngle,
-        	endAngleMin,
-        	endAngleHr;
-        
-        if (canvas.getContext) {
-            ctx.clearRect(0,0,1000,400);
-            ctx.lineCap = "round";
-            ctx.font = "20pt";
-            ctx.lineWidth = 5;
+	function polar() {
+		//variables used by all arcs
+		var timeNow = new Date(),
+			x = center.x,
+			y = center.y,
+			startAngle = (3*Math.PI/2),
+			anticlockwise = false,
+			radius,
+			radiusMin,
+			radiusHr,
+			seconds,
+			degsMin,
+			degsHr,
+			currRads,
+			radsMin,
+			radsHr,
+			endAngle,
+			endAngleMin,
+			endAngleHr;
+		
+		if (canvas.getContext) {
+			ctx.clearRect(0,0,1000,400);
+			//ctx.lineCap = "round";
+			ctx.font = "20pt";
+			ctx.lineWidth = 40;
+			//ctx.strokeStyle = '#2CDB00';
+			ctx.strokeStyle = '#57ff52';
 
-            //seconds arc
-            seconds = 6 * (timeNow.getSeconds() + (timeNow.getMilliseconds() / 1000));
-            radius = 140;
-            currRads = Math.PI*(seconds/180);
-            endAngle = currRads - Math.PI/2;
-            
-            ctx.beginPath();
-            ctx.arc(x,y,radius,startAngle,endAngle,anticlockwise);
-            ctx.stroke();
-            
-            //minutes arc
-            radiusMin = 110;
-            degsMin = 6 * (timeNow.getMinutes() + (timeNow.getSeconds() / 60) + (timeNow.getMilliseconds() / 1000 / 60));
-            radsMin = Math.PI*(degsMin/180);
-            endAngleMin = radsMin - Math.PI/2;
-            
-            ctx.beginPath();
-            ctx.arc(x,y,radiusMin,startAngle,endAngleMin,anticlockwise);
-            ctx.stroke();
-            
-            //hours arc
-            radiusHr = 80;
-            degsHr = 10.5 * ((timeNow.getHours() - 12) + (timeNow.getMinutes() / 60) + (timeNow.getSeconds() / 60 / 60) + (timeNow.getMilliseconds() / 1000 / 60 / 60));
-            radsHr = Math.PI*(degsHr/180);
-            endAngleHr = radsHr - Math.PI/2;
-            
-            ctx.beginPath();
-            ctx.arc(x,y,radiusHr,startAngle,endAngleHr,anticlockwise);
-            ctx.stroke();
-        }
-        else {
-            document.write("Canvas element unsupported...Gargantua-1 eats you.");
-        }
-    }
+			//seconds arc
+			seconds = 6 * (timeNow.getSeconds() + (timeNow.getMilliseconds() / 1000));
+			radius = 140;
+			currRads = Math.PI*(seconds/180);
+			endAngle = currRads - Math.PI/2;
+			
+			ctx.beginPath();
+			ctx.arc(x,y,radius,startAngle,endAngle,anticlockwise);
+			ctx.stroke();
+			
+			//minutes arc
+			radiusMin = 110;
+			degsMin = 6 * (timeNow.getMinutes() + (timeNow.getSeconds() / 60) + (timeNow.getMilliseconds() / 1000 / 60));
+			radsMin = Math.PI*(degsMin/180);
+			endAngleMin = radsMin - Math.PI/2;
+			ctx.strokeStyle = '#FF3333';
+			ctx.beginPath();
+			
+			ctx.arc(x,y,radiusMin,startAngle,endAngleMin,anticlockwise);
+			ctx.stroke();
+	
+			//hours arc
+			radiusHr = 80;
+			degsHr = 10.5 * ((timeNow.getHours() - 12) + (timeNow.getMinutes() / 60) + (timeNow.getSeconds() / 60 / 60) + (timeNow.getMilliseconds() / 1000 / 60 / 60));
+			radsHr = Math.PI*(degsHr/180);
+			endAngleHr = radsHr - Math.PI/2;
+			
+			ctx.beginPath();
+			ctx.strokeStyle = '#FFFF5D';
+			ctx.arc(x,y,radiusHr,startAngle,endAngleHr,anticlockwise);
+			ctx.stroke();
+		}
+	}
 }, false);
